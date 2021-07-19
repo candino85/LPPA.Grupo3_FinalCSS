03f84828d3159dea6def71fe470cf40ed22eda00
andinoc <79993810+candino85@users.noreply.github.com>
2021-07-19 16:08:51 -0300

    Add project files.

---
+++ Marketplace.Website/Controllers/AdminController.cs
@@ -0,0 +1,93 @@
+﻿using Marketplace.Business;
+using Marketplace.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marketplace.Website.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult List()
+        {
+            var biz = new LoginBiz();
+            var model = biz.List();
+            return View(model);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(LogInModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Marketplace.Website/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Marketplace.Business;
+using Marketplace.Data.Services;
+using Marketplace.Entities.Models;
+
+namespace Marketplace.Website.Controllers
+{   
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        private readonly ProductBiz _db;
+        public static List<Product> cartProducts = new List<Product>();
+
+        [HttpGet]
+        // List all of the products
+        public ActionResult Carrito(Product product)
+        {
+            if (product.CategoryId != 0)
+                cartProducts.Add(product);
+
+            return View(cartProducts);
+        }
+
+        //[HttpGet]
+        //// Clear the cart
+        //public ActionResult CarritoQuitItem(Product product)
+        //{
+        //    cartProducts.Remove(product);
+        //    return Carrito();
+        //}
+
+        //[HttpGet]
+        //// Clear the cart
+        //public ActionResult CarritoClear()
+        //{
+        //    cartProducts.Clear();
+        //    return View(cartProducts);
+        //}
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            var biz = new ProductBiz();
+            var listProducts = biz.List();
+            List<Product> listProductsCategory = new List<Product>();
+
+            foreach (var prod in listProducts)// Por cada Item en la lista 
+            {
+                if (id == prod.CategoryId)
+                {
+                    listProductsCategory.Add(prod);
+                }
+            }
+            return View(listProductsCategory);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(); // => html
+        }        
+    }
+}
